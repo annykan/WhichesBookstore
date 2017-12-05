f740cfd55010f53201f208b3dceafa9abfc33d95
Anny <anny_phison@hotmail.com>
2017-12-05 16:28:37 +0800

    加入專案檔案。

---
+++ AdminWhitches/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Web;
+using System.Web.UI;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Owin;
+using AdminWhitches.Models;
+
+namespace AdminWhitches
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
